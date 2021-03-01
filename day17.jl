6e6ff0a8bf98ed5a58c05be51ea99305a21fcaac
anto9526 <antka@student.chalmers.se>
2021-03-01 10:23:38 +0100

    ...

--- day17.jl
+++ day17.jl
@@ -1,16 +1,10 @@
 # 3d game of life infinity size 
 
-struct Cell
-    x::Int
-    y::Int
-    z::Int
-end
-
-function affected_cells(d::Dict)
+function affected_cells(living::Set)
     # Will return a set of cells that are either a living cell or adjacent to one
     aff_cells =  Set()
-    for key in keys(d)
-        (x,y,z) = key
+    for cell in living
+        (x,y,z) = cell
         for i in x-1:x+1
             for j in y-1:y+1
                 for k in z-1:z+1
@@ -21,11 +15,11 @@ function affected_cells(d::Dict)
     end
     return aff_cells
 end
-function affected_cells2(d::Dict)
+function affected_cells2(living::Set)
     # Will return a set of cells that are either a living cell or adjacent to one
     aff_cells =  Set()
-    for key in keys(d)
-        (x,y,z,w) = key
+    for cell in living
+        (x,y,z,w) = cell
         for i in x-1:x+1
             for j in y-1:y+1
                 for k in z-1:z+1
@@ -38,53 +32,53 @@ function affected_cells2(d::Dict)
     end
     return aff_cells
 end
-function nextlife(d::Dict, aff_cells)
-    new_d = Dict()
+function nextlife(living::Set, aff_cells)
+    new_life = Set()
     for cell in aff_cells
         (x,y,z) = cell
         count = 0
-        if haskey(d,cell)
+        if cell in living
             for i in x-1:x+1
                 for j in y-1:y+1
                     for k in z-1:z+1
-                        if (i,j,k) != (x,y,z) && haskey(d,(i,j,k))
+                        if (i,j,k) != (x,y,z) && (i,j,k) in living
                             count +=1
                         end 
                     end    
                 end
             end
             if 2<=count<=3
-                new_d[cell] = true
+                push!(new_life,cell)
             end
         else
             for i in x-1:x+1
                 for j in y-1:y+1
                     for k in z-1:z+1
-                        if (i,j,k) != (x,y,z) && haskey(d,(i,j,k))
+                        if (i,j,k) != (x,y,z) && (i,j,k) in living
                             count +=1
                         end 
                     end    
                 end
             end
             if count==3
-                new_d[cell] = true
+                push!(new_life,cell)
             end
         end       
     end
-    return new_d
+    return new_life
 end
 
-function nextlife2(d::Dict, aff_cells)
-    new_d = Dict()
+function nextlife2(living::Set, aff_cells)
+    new_life = Set()
     for cell in aff_cells
         (x,y,z,w) = cell
         count = 0
-        if haskey(d,cell)
+        if cell in living
             for i in x-1:x+1
                 for j in y-1:y+1
                     for k in z-1:z+1
                         for m in w-1:w+1
-                            if (i,j,k,m) != (x,y,z,w) && haskey(d,(i,j,k,m))
+                            if (i,j,k,m) != (x,y,z,w) && (i,j,k,m) in living
                                 count +=1
                             end
                         end 
@@ -92,14 +86,14 @@ function nextlife2(d::Dict, aff_cells)
                 end
             end
             if 2<=count<=3
-                new_d[cell] = true
+                push!(new_life, cell)
             end
         else
             for i in x-1:x+1
                 for j in y-1:y+1
                     for k in z-1:z+1
                         for m in w-1:w+1
-                            if (i,j,k,m) != (x,y,z,w) && haskey(d,(i,j,k,m))
+                            if (i,j,k,m) != (x,y,z,w) && (i,j,k,m) in living
                                 count +=1
                             end
                         end 
@@ -107,21 +101,21 @@ function nextlife2(d::Dict, aff_cells)
                 end
             end
             if count==3
-                new_d[cell] = true
+                push!(new_life, cell)
             end
         end       
     end
-    return new_d
+    return new_life
 end
 
 f     = open("day17_data.txt");
 lines = readlines(f)
 close(f)
-mat = Dict()
+mat = Set()
 for (i,line) in enumerate(lines)
     for (j,c) in enumerate(line)
         if c == '#'
-            mat[(j,-i,0)] = true
+            push!(mat,(j,-i,0))
         end    
     end
 end
@@ -131,15 +125,15 @@ for i in 1:6
     cells = affected_cells(mat)
     mat = nextlife(mat, cells)
 end
-println("Question 1 living cells are $(length(keys(mat)))")
+println("Question 1 living cells are $(length(mat))")
 
 
 
-mat = Dict()
+mat = Set()
 for (i,line) in enumerate(lines)
     for (j,c) in enumerate(line)
         if c == '#'
-            mat[(j,-i,0,0)] = true
+            push!(mat,(j,-i,0,0))
         end    
     end
 end
@@ -148,4 +142,4 @@ for i in 1:6
     cells = affected_cells2(mat)
     mat = nextlife2(mat, cells)
 end
-println("Question 2 living cells are $(length(keys(mat)))")
+println("Question 2 living cells are $(length(mat))")
